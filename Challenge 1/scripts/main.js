f1a1f89dff10802124c0d8aec048e2ace6105c5f
Stanko99 <kousakakirino99@hotmail.com>
2022-05-14 23:55:18 +0200

    Version 2.0

--- Challenge 1/scripts/main.js
+++ Challenge 1/scripts/main.js
@@ -1,13 +1,14 @@
 
     // Arrays with data, that will be used to displayed to the html
-    var parametersPercentage = ["10%","30%", "50%", "41%", "19%",  "29%",  "65%", "70%", "80%", "98%"];
-    var enviroment =["80%", "60%", "50%", "90%", "At full Capacity", "0%" ];
-    var metrics = ["7247445", "42069","69420", "1000", "600.000.000", "142015" ];
+    var parametersPercentage = ["100%","70% ","60% ","40% ","20% ","10% "] // Array with random % for spaceholders
     var fuel = ["100% Fuel","70% Fuel","60% Fuel","40% Fuel!","20%! Fuel","10%! Fuel"];
     var throttle = ["40% Throttle","60% Throttle","80% Throttle","100% Throttle"];
     var kmph = ["4486 kmph","5210 kmph","5555 kmph","6001 kmph"];
     var outcome = ["Sucess!", "Chance to fail mission 70%", "Emergency stop", "Asteroid Incoming", "Death"];
     var temperatures = ["-270C","-350C","-180C","-220C","450C"];
+    var damage =["15% Damaged","30% Damaged","60% Danger!","80%!! Danger","Evacuate!!!"]
+    var gravity =["620.389 * sg2","648.670 * sg2", "(80% AA): 5.35%","SG -775.6821" ] 
+    var distance =["232.96M","160.21M","60.12M","120.28M","90.52M","220.78M","50.11M"]
 
 
     function paraFunction (){
@@ -16,57 +17,55 @@
         var parametersResult = parametersPercentage[Math.floor(Math.random()*parametersPercentage.length)];
         //making a var that will randomly select an element from the first array 
         //using the Math.random to select an arbitrary number from 0-1 and then Math.floor to round the number, then multiple it by the lenth of the array
-        document.getElementById("damage").innerHTML = parametersResult
-        //chaning the html element with "parameters" id, to the variable 
+        document.getElementById("").innerHTML = parametersResult }
+        //chaning the html element with "parameters" id, to the variable
+        //I am not using anywhere the above function 
 
-        var parametersResult = parametersPercentage[Math.floor(Math.random()*parametersPercentage.length)];
-        document.getElementById("oxygen-cap").innerHTML = parametersResult };
-        //Throttle 
-
-        var parametersResult = parametersPercentage[Math.floor(Math.random()*parametersPercentage.length)];
-        document.getElementById("speed").innerHTML = parametersResult 
-        //Speed
-
-        var temperaturesView = temperatures[Math.floor(Math.random()*temperatures.length)];
-        document.getElementById("temperature").innerHTML = temperaturesView
-        //Acceleration
 
         var hudView = fuel[Math.floor(Math.random()*fuel.length)];
-        document.getElementById("fuel").innerHTML = hudView
-         // HUD array, circles for fuel
+        document.getElementById("fuel").innerHTML = hudView 
+        // HUD array, circles for fuel
 
         var hudView = throttle[Math.floor(Math.random()*throttle.length)];
         document.getElementById("rpm").innerHTML = hudView
          // HUD array, circles for throttle
 
-        var hudView = kmph[Math.floor(Math.random()*kmph.length)];
+         var hudView = kmph[Math.floor(Math.random()*kmph.length)];
         document.getElementById("km/h").innerHTML = hudView
-             // HUD array, circles for speed
+        // HUD array, circles for speed
 
+        var damageView = damage[Math.floor(Math.random()*damage.length)];
+        document.getElementById("taken-dmg").innerHTML = damageView
+         // HUD array, circles for fuel
 
-        var parametersResult = parametersPercentage[Math.floor(Math.random()*parametersPercentage.length)];
-        document.getElementById("power").innerHTML = parametersResult
-        //power usage, 
-
-        var enviromentResult = enviroment[Math.floor(Math.random()*enviroment.length)];
+        var gravityView = gravity[Math.floor(Math.random()*gravity.length)];
         //Making another variable for the next array
-        document.getElementById("gravity").innerHTML = enviromentResult 
+        document.getElementById("gravity-cap").innerHTML = gravityView 
+
+        var temperaturesView = temperatures[Math.floor(Math.random()*temperatures.length)];
+        document.getElementById("temperature").innerHTML = temperaturesView
         //Gravity
 
-        var enviromentResult = enviroment[Math.floor(Math.random()*enviroment.length)];
+        var enviromentResult = parametersPercentage[Math.floor(Math.random()*parametersPercentage.length)];
         document.getElementById("food").innerHTML = enviromentResult 
         //Food
 
-        var enviromentResult = enviroment[Math.floor(Math.random()*enviroment.length)];
-        document.getElementById("water").innerHTML = enviromentResult 
+        var enviromentResult = parametersPercentage[Math.floor(Math.random()*parametersPercentage.length)];
+        document.getElementById("water").innerHTML = enviromentResult
         //Water
 
-        var metricsResult = metrics[Math.floor(Math.random()*metrics.length)];
-        document.getElementById("mars").innerHTML = metricsResult 
+        var  viewDistance = distance[Math.floor(Math.random()*distance.length)];
+        document.getElementById("distance-mars").innerHTML = viewDistance
         //Mars-Distance
 
         var outcomeView = outcome[Math.floor(Math.random()*outcome.length)];
         document.getElementById("outcome").innerHTML = outcomeView
+
+
+
+
+
+        //Acceleration
 
 
     paraFunction();
